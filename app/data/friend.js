0cef106cfd291755efa3b9048fd139046cbcb179
SHollatz <ztallohs@gmail.com>
2018-08-17 15:33:37 -0700

    finished matching logic

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,7 +1,7 @@
 module.exports = [
     {
         "name": "Chelsey",
-        "photo": "./app/images/Chelsey.jpg",
+        "photo": "https://github.com/SHollatz/friendfinder/blob/master/app/images/Chelsey_alias.jpg?raw=true",
         "scores": [
             5,
             1,
@@ -13,6 +13,150 @@ module.exports = [
             5,
             4,
             1
+        ]
+    },
+    {
+        "name": "Clara",
+        "photo": "https://github.com/SHollatz/friendfinder/blob/master/app/images/Clara_alias.jpg?raw=true",
+        "scores": [
+            4,
+            5,
+            4,
+            3,
+            4,
+            2,
+            1,
+            4,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Hannah",
+        "photo": "https://github.com/SHollatz/friendfinder/blob/master/app/images/Hannah_alias.jpg?raw=true",
+        "scores": [
+            2,
+            5,
+            3,
+            5,
+            2,
+            3,
+            4,
+            1,
+            3,
+            4
+        ]
+    },
+    {
+        "name": "Isaac",
+        "photo": "https://github.com/SHollatz/friendfinder/blob/master/app/images/Isaac_alias.jpg?raw=true",
+        "scores": [
+            2,
+            3,
+            2,
+            5,
+            4,
+            3,
+            1,
+            1,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Joaquim",
+        "photo": "https://github.com/SHollatz/friendfinder/blob/master/app/images/Joaquim_alias.jpg?raw=true",
+        "scores": [
+            3,
+            3,
+            4,
+            5,
+            2,
+            3,
+            4,
+            1,
+            3,
+            2
+        ]
+    },
+    {
+        "name": "John",
+        "photo": "https://github.com/SHollatz/friendfinder/blob/master/app/images/John_alias.jpg?raw=true",
+        "scores": [
+            1,
+            2,
+            4,
+            3,
+            5,
+            4,
+            3,
+            1,
+            2,
+            4
+        ]
+    },
+    {
+        "name": "Lila",
+        "photo": "https://github.com/SHollatz/friendfinder/blob/master/app/images/Lila_alias.jpg?raw=true",
+        "scores": [
+            3,
+            3,
+            4,
+            2,
+            3,
+            2,
+            4,
+            3,
+            3,
+            2
+        ]
+    },
+    {
+        "name": "Mat",
+        "photo": "https://github.com/SHollatz/friendfinder/blob/master/app/images/Clara_alias.jpg?raw=true",
+        "scores": [
+            5,
+            2,
+            3,
+            1,
+            4,
+            5,
+            3,
+            4,
+            2,
+            4
+        ]
+    },
+    {
+        "name": "Miranda",
+        "photo": "https://github.com/SHollatz/friendfinder/blob/master/app/images/Miranda_alias.jpg?raw=true",
+        "scores": [
+            3,
+            3,
+            1,
+            2,
+            5,
+            1,
+            2,
+            5,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "Scott",
+        "photo": "https://github.com/SHollatz/friendfinder/blob/master/app/images/Scott_alias.jpg?raw=true",
+        "scores": [
+            5,
+            3,
+            4,
+            2,
+            3,
+            1,
+            1,
+            2,
+            4,
+            5
         ]
     },
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,8 +9,10 @@ function calculateMatch(arrayDB, arrayNewUser) {
         } else {
             singleResult = parseInt(arrayDB[m]) - parseInt(arrayNewUser[m]);
         }
+        //console.log("singleResult " + singleResult);
         totalResult = totalResult + singleResult;      
     }
+    //console.log("totalResult " + totalResult);
     return totalResult;
 }
 
@@ -20,18 +22,21 @@ module.exports = function (app) {
     });
     app.post('/api/friends', function(req, res) {
         var newUser = req.body;
-        console.log("req.body " + req.body);
+        //console.log("req.body " + req.body);
         var matchResults = [];
 
         for (var i = 0; i < friendsData.length; i++) {
-            matchResults.push(calculateMatch(friendsData[i].scores, newUser.scores));        
+            matchResults.push(calculateMatch(friendsData[i].scores, newUser.scores));
+                    
         }
+        //console.log("matchResults " + matchResults);
         var smallestDiff = 51;
         for (var j = 0; j < matchResults.length; j++) {
             if (matchResults[j] < smallestDiff) {
                 smallestDiff = matchResults[j];
             }
         }
+        //console.log("smallesDiff " + smallestDiff);
         var index = matchResults.indexOf(smallestDiff);
         res.json(friendsData[index]);
     });
